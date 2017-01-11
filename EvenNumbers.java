8fc5b51ba2cb529d25bf5c20a0cb0bd6c74d1092
shanusuresh <shanmugapriya.sabapathy@gmail.com>
2017-01-11 12:55:35 +0530

    Add files via upload

--- EvenNumbers.java
+++ EvenNumbers.java
@@ -1,1 +1,23 @@
-//Write program to print even numbers between  100 and 200
+/*even numbers between 100 to 200
+
+for(i=100;i<=200;i++)
+if (i%2==0) then print the number as even
+*/
+
+public class EvenNumbers{
+	
+	public static void main(String[] args)
+	{
+		int num;
+		System.out.println("Even numbers between 100 and 200 are: ");
+		for(int i=100;i<=200;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i);
+			}
+		}
+		
+	}
+}
+
